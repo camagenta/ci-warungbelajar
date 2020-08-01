56a6ba35bb95aebfd07691540bb4ca6b9a3e24d9
Kurnia Adhiwibowo <camagenta@gmail.com>
2020-08-02 06:31:52 +0700

    tutorial 8

---
+++ application/views/vw_testing.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Belajar Buat Helper</title>
+    </head>
+    <body>
+        <h3>Belajar Membuat Helper</h3>
+        <hr/>
+        Hari ini tanggal <?php echo tanggal('2019-10-29'); ?>
+    </body>
+</html>
